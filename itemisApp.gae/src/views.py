bbc22eca94532097fec95bf726b83f21099ab297
Heiko Behrens <HeikoBehrens@gmx.de>
2011-03-19 21:20:29 +0100

    moved generated templates into separate folder

--- itemisApp.gae/src/views.py
+++ itemisApp.gae/src/views.py
@@ -3,21 +3,21 @@ import content as providers
 
 def allSpeakers(request):
     data = providers.allSpeakers()
-    return render_to_response('_allSpeakers.html', {'data': data})
+    return render_to_response('generated/allSpeakers.html', {'data': data})
 
 def speaker_resolver(request, id):
     data = providers.speakerById(id)
-    return render_to_response('_speaker.html', {'data': data})
+    return render_to_response('generated/speaker.html', {'data': data})
 
 def speaker_byName(request, name):
     data = providers.speakerByName(name)
-    return render_to_response('_speaker.html', {'data': data})
+    return render_to_response('generated/speaker.html', {'data': data})
 
 def session_resolver(request, id):
     data = providers.sessionById(id)
-    return render_to_response('_session.html', {'data': data})
+    return render_to_response('generated/session.html', {'data': data})
     
 def sessions_byDay(request, day):
     data = providers.sessionsByDay(day)
-    return render_to_response('_sessions.html', {'data': data})
+    return render_to_response('generated/sessions.html', {'data': data})
     
