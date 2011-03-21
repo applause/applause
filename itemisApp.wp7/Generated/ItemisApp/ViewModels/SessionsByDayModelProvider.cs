a2149e14859172d29ef5dfdfa2798301c287565d
Peter Friese <peter.friese@itemis.de>
2011-03-21 01:10:47 +0100

    WP7 Project

---
+++ itemisApp.wp7/Generated/ItemisApp/ViewModels/SessionsByDayModelProvider.cs
@@ -0,0 +1,79 @@
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.Net;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ItemisApp.ViewModels
+{
+	public class SessionsByDayModelProvider
+	{
+		
+		private String _day;
+		
+		public SessionsByDayModelProvider(String day)
+		{
+			_day = day;
+			this.Sessions = new ObservableCollection<Session>();
+		}
+				
+		
+		public ObservableCollection<Session> Sessions { get; private set; }
+		
+		public bool IsDataLoaded
+		{
+			get;
+			private set;
+		}
+		
+		public void LoadData()
+		{
+			WebClient client = new WebClient();
+			client.DownloadStringAsync(new Uri("http://192.168.210.1:3000" + "/sessions/day/" + day + ".xml"));
+			client.DownloadStringCompleted += new DownloadStringCompletedEventHandler(client_DownloadStringCompleted);			
+		}
+		
+		void client_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+		{
+			if (e.Error == null)
+			{
+				ParseDataFromXml(e.Result);
+				this.IsDataLoaded = true;
+			}
+		}
+
+		void ParseDataFromXml(String source)
+		{
+			this.Sessions.Clear();
+
+			XDocument xdoc = XDocument.Parse(source);
+			XNamespace dc ="http://purl.org/dc/elements/1.1/";
+			List<Session> result = 
+				(
+					from item in xdoc.Descendants("sessions.session")
+					select new Session
+					{
+						Id = item.Element("id").Value,
+						Title = item.Element("title").Value,
+						Description = item.Element("description").Value,
+						Timeslot = item.Element("timeslot").Value,
+						Room = item.Element("room").Value,
+						Speakers = item.Element("speakers").Value,
+					}
+				).ToList<Session>();
+			result.ForEach(this.Sessions.Add);
+		}
+	}
+}
---
+++ itemisApp.wp7/Generated/ItemisApp/ViewModels/Speaker.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace ItemisApp.ViewModels
+{
+	public class Speaker : INotifyPropertyChanged
+	{
+		
+		private String _id;
+
+		public String Id
+		{
+			get { return _id; }
+			set
+			{
+				if (value != _id)
+				{
+					_id = value;
+					NotifyPropertyChanged("Id");
+				}
+			}
+		}
+
+		private String _name;
+
+		public String Name
+		{
+			get { return _name; }
+			set
+			{
+				if (value != _name)
+				{
+					_name = value;
+					NotifyPropertyChanged("Name");
+				}
+			}
+		}
+
+		private String _bio;
+
+		public String Bio
+		{
+			get { return _bio; }
+			set
+			{
+				if (value != _bio)
+				{
+					_bio = value;
+					NotifyPropertyChanged("Bio");
+				}
+			}
+		}
+
+		private String _pictureurl;
+
+		public String Pictureurl
+		{
+			get { return _pictureurl; }
+			set
+			{
+				if (value != _pictureurl)
+				{
+					_pictureurl = value;
+					NotifyPropertyChanged("Pictureurl");
+				}
+			}
+		}
+
+		private Session _sessions;
+
+		public Session Sessions
+		{
+			get { return _sessions; }
+			set
+			{
+				if (value != _sessions)
+				{
+					_sessions = value;
+					NotifyPropertyChanged("Sessions");
+				}
+			}
+		}
+
+		
+		// PF: Note to self: this really should be in a runtime class
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void NotifyPropertyChanged(String propertyName)
+		{
+			PropertyChangedEventHandler handler = PropertyChanged;
+			if (null != handler)
+			{
+				handler(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
