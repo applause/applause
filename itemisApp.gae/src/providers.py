282ef4de46ea449fa6264730049951d162691ae2
Heiko Behrens <HeikoBehrens@gmx.de>
2011-03-20 22:30:45 -0700

    webapp data from webby-server

--- itemisApp.gae/src/providers.py
+++ itemisApp.gae/src/providers.py
@@ -7,37 +7,37 @@ def plainxml(url):
 
 
 def Blogposts():
-    url = 'http://feedsanitizer.appspot.com/sanitize?url=http%3A%2F%2Fblogs.itemis.de%2F%3Fshowfeed%3D1&format=rss' #foo
+    url = '%s%s' % ('http://feedsanitizer.appspot.com', '/sanitize?url=http%3A%2F%2Fblogs.itemis.de%2F%3Fshowfeed%3D1&format=rss') #foo
     xml = plainxml(url)
     return xml.channel.item
 
 
 def SessionsByDay(day):
-    url = '%s%s%s%s' % ('http://localhost:3000', '/sessions/day/', day, '.xml') #foo
+    url = '%s%s%s%s' % ('http://eclipsecon2011-data.webbyapp.com', '/sessions/day/', day, '.xml') #foo
     xml = plainxml(url)
     return xml.session
 
 
 def AllSpeakers():
-    url = '%s%s' % ('http://localhost:3000', '/speakers.xml') #foo
+    url = '%s%s' % ('http://eclipsecon2011-data.webbyapp.com', '/speakers.xml') #foo
     xml = plainxml(url)
     return xml.speaker
 
 
 def BlogItemById(guid):
-    url = '%s%s' % ('http://feedsanitizer.appspot.com/sanitize?url=http%3A%2F%2Fblogs.itemis.de%2F%3Fshowfeed%3D1&format=rss&id=', quote(guid)) #foo
+    url = '%s%s%s' % ('http://feedsanitizer.appspot.com', '/sanitize?url=http%3A%2F%2Fblogs.itemis.de%2F%3Fshowfeed%3D1&format=rss&id=', quote(guid)) #foo
     xml = plainxml(url)
     return xml.channel.item
 
 
 def SpeakerByName(name):
-    url = '%s%s%s%s' % ('http://localhost:3000', '/speakers/name/', quote(name), '.xml') #foo
+    url = '%s%s%s%s' % ('http://eclipsecon2011-data.webbyapp.com', '/speakers/name/', quote(name), '.xml') #foo
     xml = plainxml(url)
     return xml.speaker
 
 
 def SessionById(id):
-    url = '%s%s%s%s' % ('http://localhost:3000', '/sessions/id/', id, '.xml') #foo
+    url = '%s%s%s%s' % ('http://eclipsecon2011-data.webbyapp.com', '/sessions/id/', id, '.xml') #foo
     xml = plainxml(url)
     return xml.session
 
