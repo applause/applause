9155430e290975a464e9c8890b2220fbf36f0fd9
Heiko Behrens <HeikoBehrens@gmx.de>
2011-03-22 15:45:16 -0700

    final version of GAE

--- itemisApp.gae/src/providers.py
+++ itemisApp.gae/src/providers.py
@@ -13,13 +13,13 @@ def Blogposts():
 
 
 def SessionsByDay(day):
-    url = '%s%s%s%s' % ('http://localhost:3000', '/sessions/day/', day, '.xml') #foo
+    url = '%s%s%s%s' % ('http://eclipsecon2011-data.webbyapp.com', '/sessions/day/', day, '.xml') #foo
     xml = plainxml(url)
     return xml.session
 
 
 def AllSpeakers():
-    url = '%s%s' % ('http://localhost:3000', '/speakers.xml') #foo
+    url = '%s%s' % ('http://eclipsecon2011-data.webbyapp.com', '/speakers.xml') #foo
     xml = plainxml(url)
     return xml.speaker
 
@@ -31,13 +31,13 @@ def BlogItemById(guid):
 
 
 def SpeakerByName(name):
-    url = '%s%s%s%s' % ('http://localhost:3000', '/speakers/name/', quote(name), '.xml') #foo
+    url = '%s%s%s%s' % ('http://eclipsecon2011-data.webbyapp.com', '/speakers/name/', quote(name), '.xml') #foo
     xml = plainxml(url)
     return xml.speaker
 
 
 def SessionById(id):
-    url = '%s%s%s%s' % ('http://localhost:3000', '/sessions/id/', id, '.xml') #foo
+    url = '%s%s%s%s' % ('http://eclipsecon2011-data.webbyapp.com', '/sessions/id/', id, '.xml') #foo
     xml = plainxml(url)
     return xml.session
 
