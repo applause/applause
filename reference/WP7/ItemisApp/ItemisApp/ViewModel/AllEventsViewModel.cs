df0c6846f0cdc11f54875fcf3ca67bbf0fd26bd9
Peter Friese <peter@peterfriese.de>
2011-09-05 12:18:52 +0200

    Implemented grouping for view "News".

--- reference/WP7/ItemisApp/ItemisApp/ViewModel/AllEventsViewModel.cs
+++ reference/WP7/ItemisApp/ItemisApp/ViewModel/AllEventsViewModel.cs
@@ -5,6 +5,8 @@ using System.Windows;
 using System.Xml.Linq;
 using GalaSoft.MvvmLight;
 using ItemisApp.Model;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace ItemisApp.ViewModel
 {
@@ -101,6 +103,11 @@ namespace ItemisApp.ViewModel
             Deployment.Current.Dispatcher.BeginInvoke(() =>
             {
                 AllEvents = fetchResult.First();
+                var grouped = new List<GroupingLayer<string, Event>>();
+                grouped.Add(new GroupingLayer<string, Event>("News", AllEvents.News));
+                grouped.Add(new GroupingLayer<string, Event>("Events", AllEvents.Activity));
+                grouped.Add(new GroupingLayer<string, Event>("Workshops", AllEvents.Workshop));
+                AllEventsGrouped = grouped;
             });
 
         }
@@ -109,8 +116,10 @@ namespace ItemisApp.ViewModel
         /// The <see cref="AllEvents" /> property's name.
         /// </summary>
         public const string AllEventsPropertyName = "AllEvents";
+        public const string AllEventsGroupedProperyName = "AllEventsGrouped";
 
         private AllEvents _allEvents = null;
+        List<GroupingLayer<string, Event>> _grouped = null;
 
         /// <summary>
         /// Gets the AllEvents property.
@@ -137,6 +146,27 @@ namespace ItemisApp.ViewModel
 
                 // Update bindings, no broadcast
                 RaisePropertyChanged(AllEventsPropertyName);
+            }
+        }
+
+        public List<GroupingLayer<string, Event>> AllEventsGrouped
+        {
+            get
+            {
+                return _grouped;
+            }
+            set
+            {
+                if (_grouped == value)
+                {
+                    return;
+                }
+
+                var oldValue = _grouped;
+                _grouped = value;
+
+                // Update bindings, no broadcast
+                RaisePropertyChanged(AllEventsGroupedProperyName);
             }
         }
 
---
+++ reference/WP7/ItemisApp/ItemisApp/ViewModel/GroupingLayer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Linq;
+using ItemisApp.Model;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ItemisApp.ViewModel
+{
+    public class GroupingLayer<TKey, TElement> : IGrouping<TKey, TElement>
+    {
+        public TKey Key { get; private set; }
+        private IEnumerable<TElement> Collection { get; set; }
+
+        public GroupingLayer(TKey key, IEnumerable<TElement> collection)
+        {
+            Key = key;
+            Collection = collection;
+        }
+
+        public IEnumerator<TElement> GetEnumerator()
+        {
+            return Collection.GetEnumerator();
+        }
+
+        IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return Collection.GetEnumerator();
+        }
+
+    }
+}
