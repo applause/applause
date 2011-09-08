8d3499fb3f3abdd2a26a344717198994d69f2df9
Peter Friese <peter@peterfriese.de>
2011-09-09 01:29:49 +0200

    Added gestures to enable "tap" events.

---
+++ reference/WP7/ItemisApp/ItemisApp/Interactivity/TapTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ItemisApp.Interactivity
+{
+    public class TapTrigger : GestureTrigger
+    {
+        protected override void Listen(Microsoft.Phone.Controls.GestureListener listener)
+        {
+            listener.Tap += new EventHandler<Microsoft.Phone.Controls.GestureEventArgs>(listener_Tap);
+        }
+
+        protected override void EndListen(Microsoft.Phone.Controls.GestureListener listener)
+        {
+            listener.Tap -= new EventHandler<Microsoft.Phone.Controls.GestureEventArgs>(listener_Tap);
+        }
+
+        void listener_Tap(object sender, Microsoft.Phone.Controls.GestureEventArgs e)
+        {
+            Invoke(e);
+        }
+    }
+}
