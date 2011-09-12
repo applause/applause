10297f5e81b117de4176416d77c8b14a4a8cd8e3
Peter Friese <peter@peterfriese.de>
2011-09-12 16:35:30 +0200

    WP7 Generator: DetailsView and MainView.

--- tooling/org.applause.lang.generator.wp7/src/templates/views/Extensions.java
+++ tooling/org.applause.lang.generator.wp7/src/templates/views/Extensions.java
@@ -1,4 +1,4 @@
-package templates.viewmodels;
+package templates.views;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -7,8 +7,11 @@ import java.util.Set;
 
 import org.applause.lang.applauseDsl.ComplexProviderConstruction;
 import org.applause.lang.applauseDsl.ContentProvider;
+import org.applause.lang.applauseDsl.SectionCell;
+import org.applause.lang.applauseDsl.TypeDescription;
 import org.applause.lang.applauseDsl.View;
 import org.applause.lang.applauseDsl.ViewCall;
+import org.applause.lang.scoping.TypeUtil;
 import org.eclipse.xtext.util.Pair;
 import org.eclipse.xtext.util.Tuples;
 
@@ -32,6 +35,27 @@ public class Extensions {
 		for(ViewCall vc : Iterables.filter(calls, ViewCall.class))
 			if(keys.add(key(vc)))
 				result.add(vc);
+		
+		return result;
+	}
+	
+	private static Pair<String, String> key(SectionCell cell) {
+		TypeDescription textType = org.applause.lang.scoping.TypeUtil.getTypeOf(cell.getText());
+		TypeDescription cellType = TypeUtil.getTypeOf(cell.getIterator());
+		System.out.println("Tuple: " + textType.getType().getName() + "::" + cellType.getType().getName());
+		return Tuples.create(cellType.getType().getName(), textType.getType().getName());
+	}
+	
+	public static List<SectionCell> uniqueSectionCells(@SuppressWarnings("rawtypes") List cells) {
+		List<SectionCell> result = new ArrayList<SectionCell>();
+		Set<Pair<String, String>> keys = new HashSet<Pair<String, String>>();
+		
+		for(SectionCell cell: Iterables.filter(cells, SectionCell.class)) {
+			if(keys.add(key(cell))) {
+				System.out.println("Adding");
+				result.add(cell);
+			}
+		}
 		
 		return result;
 	}
